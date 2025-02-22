93bdb27ddcb3a6a6ba96d642e32302b2268e6129
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2025-02-22 18:37:59 -0500

    Created backend with spring initilizr

---
+++ backend/src/test/java/com/picturest/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picturest.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
